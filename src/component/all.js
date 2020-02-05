85407259cb5d2e8b29b01fb6b7bb65599c9bd5e4
Omar Estrada Diaz <omarito4857@gmail.com>
2020-02-05 16:40:15 -0600

    before to upload to github

--- src/component/all.js
+++ src/component/all.js
@@ -1,26 +1,73 @@
 import React from 'react';
-import Bici from '../img/bici'
-import '../App.css'
+import Bici from '../img/bici';
+import { makeStyles } from '@material-ui/styles';
 
-const All = () => (
-        <div style={styleImage}>
-                <Bici   width={500} height={500}/>
-                <p>Error</p>
-                
-        </div >
-)
+
+const All = () => {
+        const stylos = useStyles();
+
+        return (
+                <div >
+                        <div className={stylos.fondo}>
+
+                                <Bici  className={stylos.bici}/>
+                                {/* <p>Error</p> */}
+
+                        </div>
+                </div >)
+
+}
+
+
 
 export default All
 
+const useStyles = makeStyles({
+        bici: {
+                display: 'flex',
+                marginLeft:'5%',
+                // alignContent:'stretch',
+                // position: 'absolute'
+        },
+        fondo: {
+                backgroundImage: "url(http://localhost/img/fondo.png)",
+                display: 'flex',
+                paddingTop: '10%',
+                // paddingBottom:'10%',
+                // position: 'absolute'
+                // paddingLeft:'2%',
+                // width: '100%',
+                height:'100%',
+                // alignItems:'flexStart'
+        },
+        price: {
+                marginRight: '2rem'
+        },
+        submit: {
+                display: 'flex',
+                justifyContent: 'flex-end',
+                alignItems: 'center'
+        },
+        
+});
 
-const styleImage = {  
+
+// const useStyles = makeStyles({
+//         styleImagee : {
+//          backgroundImage: 'url(http://localhost/img/fondo.png)'
+//         }
+// })
+
+const styleImage = {
         backgroundImage: "url(http://localhost/img/fondo.png)",
-        width:"100",
-        height:"100"
+        width: "100%",
+        height: "100%"
         // backgroundPosition: 'rigth',
         // backgroundSize: 'cover',
         // backgroundRepeat: 'no-repeat'
-      }
+}
+
+
 
 
 //       src="http://localhost/img/fondo.png"
